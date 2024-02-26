427af98160b5b14b5eb8ac7de40068275ee972be
Benedicte Emly Solberg Johansen <138583584+Benedicte-Johansen@users.noreply.github.com>
2024-02-26 13:39:54 +0100

    Legg til filer og kode

---
+++ OppgaverJS1.js
@@ -0,0 +1,13 @@
+function visHeiVerden(){
+//document.write("Hei verden!");
+    alert("Hei verden!");
+}
+
+function infoUt(){
+    let innInfo = document.getElementById("infoInn").value;
+
+    document.write(innInfo);
+    //alert(innInfo);
+
+    console.log(innInfo);
+}
